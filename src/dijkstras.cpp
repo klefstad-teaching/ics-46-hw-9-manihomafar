8d65d73ba3feb0f3869235d82915754ea9b2ccb6
Mani Homafar <146906257+manihomafar@users.noreply.github.com>
2025-03-17 02:39:05 +0000

    Updated extractshortestpath

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -31,6 +31,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     return distances;
 }
 vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination){
+    vector<int> path;
     if (previous[destination] == -1) {
         return path;
     }
